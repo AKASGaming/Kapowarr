a5bcf943ddf84c6e6b7499545df9a307aca421e4
CasVT <casvantijn@gmail.com>
2025-05-22 22:27:11 +0200

    Converted manifest file into dynamic endpoint

--- frontend/ui.py
+++ frontend/ui.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from io import BytesIO
+from json import dumps
 from typing import Any
 
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, send_file
 
 from backend.internals.server import SERVER
 
@@ -12,6 +14,36 @@ methods = ['GET']
 
 def render(filename: str, **kwargs: Any) -> str:
     return render_template(filename, url_base=SERVER.url_base, **kwargs)
+
+
+@ui.route('/manifest.json', methods=methods)
+def ui_manifest():
+    return send_file(
+        BytesIO(dumps(
+            {
+                "name": "Kapowarr",
+                "short_name": "Kapowarr",
+                "description": "Kapowarr is a software to build and manage a comic book library, fitting in the *arr suite of software.",
+                "display": "standalone",
+                "orientation": "portrait-primary",
+                "start_url": f"{SERVER.url_base}/",
+                "scope": f"{SERVER.url_base}/",
+                "id": f"{SERVER.url_base}/",
+                "background_color": "#464b51",
+                "theme_color": "#ebc700",
+                "icons": [
+                    {
+                        "src": f"{SERVER.url_base}/static/img/favicon.svg",
+                        "type": "image/svg+xml",
+                        "sizes": "any"
+                    }
+                ]
+            },
+            indent=4
+        ).encode('utf-8')),
+        mimetype="application/manifest+json",
+        download_name="manifest.json"
+    ), 200
 
 
 @ui.route('/login', methods=methods)
