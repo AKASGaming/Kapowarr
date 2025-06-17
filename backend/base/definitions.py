b19ece35a77ed4528cbed69b6e89603be31f3b4c
CasVT <casvantijn@gmail.com>
2025-06-17 15:02:14 +0200

    Fixed loading downloads after restart
    Identifier property obviously requires the class to be instantiated but that's not possible at that stage so changed it back to just a normal class attribute.

--- backend/base/definitions.py
+++ backend/base/definitions.py
@@ -962,11 +962,8 @@ class ExternalDownloadClient(ABC):
 
 
 class Download(ABC):
-    @property
-    @abstractmethod
-    def identifier(self) -> str:
-        "An identifier for the specific download implementation (e.g. 'mf_folder')"
-        ...
+    identifier: str
+    "An identifier for the specific download implementation (e.g. 'mf_folder')"
 
     @property
     @abstractmethod
