c981c15162b46fa864b6eb07c1e59bd98673a4e7
CasVT <casvantijn@gmail.com>
2025-07-29 14:28:35 +0200

    Fixed searching GC via FS
    We would send the URL to FlareSolverr, but without the URL parameter for the query.

--- backend/base/helpers.py
+++ backend/base/helpers.py
@@ -959,7 +959,8 @@ class AsyncSession(ClientSession):
                 LOGGER.debug(
                     'Made async request: %s "%s" %d %d',
                     method, response.url,
-                    response.status, response.headers.get('Content-Length', -1)
+                    response.status,
+                    int(response.headers.get('Content-Length', -1))
                 )
 
                 if response.status in Constants.STATUS_FORCELIST_RETRIES:
@@ -987,7 +988,7 @@ class AsyncSession(ClientSession):
                 and response.status == 403
             ):
                 fs_result = await self.fs.handle_cf_block_async(
-                    self, url, response.headers
+                    self, str(response.url), response.headers
                 )
 
                 if not fs_result:
